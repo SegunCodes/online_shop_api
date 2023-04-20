ab5accb815144c9a1bdde155c6051aacb9a170ff
SegunCodes <shegstix64@gmail.com>
2023-04-20 16:14:23 +0200

    clean up customer service area and add message broker integration

--- microservice/customer/src/database/repository/customer-repository.js
+++ microservice/customer/src/database/repository/customer-repository.js
@@ -1,223 +1,177 @@
-const { CustomerModel, AddressModel } = require("../models");
-const {
-  APIError,
-  BadRequestError,
-  STATUS_CODES,
-} = require("../../utils/app-errors");
+const mongoose = require('mongoose');
+const { CustomerModel, AddressModel } = require('../models');
 
 //Dealing with data base operations
 class CustomerRepository {
-  async CreateCustomer({ email, password, phone, salt }) {
-    try {
-      const customer = new CustomerModel({
-        email,
-        password,
-        salt,
-        phone,
-        address: [],
-      });
-      const customerResult = await customer.save();
-      return customerResult;
-    } catch (err) {
-      throw new APIError(
-        "API Error",
-        STATUS_CODES.INTERNAL_ERROR,
-        "Unable to Create Customer"
-      );
+
+    async CreateCustomer({ email, password, phone, salt }){
+
+        const customer = new CustomerModel({
+            email,
+            password,
+            salt,
+            phone,
+            address: []
+        })
+
+        const customerResult = await customer.save();
+        return customerResult;
     }
-  }
+    
+    async CreateAddress({ _id, street, postalCode, city, country}){
+        
+        const profile = await CustomerModel.findById(_id);
+        
+        if(profile){
+            
+            const newAddress = new AddressModel({
+                street,
+                postalCode,
+                city,
+                country
+            })
 
-  async CreateAddress({ _id, street, postalCode, city, country }) {
-    try {
-      const profile = await CustomerModel.findById(_id);
+            await newAddress.save();
 
-      if (profile) {
-        const newAddress = new AddressModel({
-          street,
-          postalCode,
-          city,
-          country,
-        });
-
-        await newAddress.save();
-
-        profile.address.push(newAddress);
-      }
-
-      return await profile.save();
-    } catch (err) {
-      throw new APIError(
-        "API Error",
-        STATUS_CODES.INTERNAL_ERROR,
-        "Error on Create Address"
-      );
-    }
-  }
-
-  async FindCustomer({ email }) {
-    try {
-      const existingCustomer = await CustomerModel.findOne({ email: email });
-      return existingCustomer;
-    } catch (err) {
-      throw new APIError(
-        "API Error",
-        STATUS_CODES.INTERNAL_ERROR,
-        "Unable to Find Customer"
-      );
-    }
-  }
-
-  async FindCustomerById({ id }) {
-    try {
-      const existingCustomer = await CustomerModel.findById(id)
-        .populate("address")
-        .populate("wishlist")
-        .populate("orders")
-        .populate("cart.product");
-      return existingCustomer;
-    } catch (err) {
-      throw new APIError(
-        "API Error",
-        STATUS_CODES.INTERNAL_ERROR,
-        "Unable to Find Customer"
-      );
-    }
-  }
-
-  async Wishlist(customerId) {
-    try {
-      const profile = await CustomerModel.findById(customerId).populate(
-        "wishlist"
-      );
-
-      return profile.wishlist;
-    } catch (err) {
-      throw new APIError(
-        "API Error",
-        STATUS_CODES.INTERNAL_ERROR,
-        "Unable to Get Wishlist "
-      );
-    }
-  }
-
-  async AddWishlistItem(customerId, product) {
-    try {
-      const profile = await CustomerModel.findById(customerId).populate(
-        "wishlist"
-      );
-
-      if (profile) {
-        let wishlist = profile.wishlist;
-
-        if (wishlist.length > 0) {
-          let isExist = false;
-          wishlist.map((item) => {
-            if (item._id.toString() === product._id.toString()) {
-              const index = wishlist.indexOf(item);
-              wishlist.splice(index, 1);
-              isExist = true;
-            }
-          });
-
-          if (!isExist) {
-            wishlist.push(product);
-          }
-        } else {
-          wishlist.push(product);
+            profile.address.push(newAddress);
         }
 
-        profile.wishlist = wishlist;
-      }
-
-      const profileResult = await profile.save();
-
-      return profileResult.wishlist;
-    } catch (err) {
-      throw new APIError(
-        "API Error",
-        STATUS_CODES.INTERNAL_ERROR,
-        "Unable to Add to WishList"
-      );
+        return await profile.save();
     }
-  }
 
-  async AddCartItem(customerId, product, qty, isRemove) {
-    try {
-      const profile = await CustomerModel.findById(customerId).populate(
-        "cart.product"
-      );
+    async FindCustomer({ email }){
+        const existingCustomer = await CustomerModel.findOne({ email: email });
+        return existingCustomer;
+    }
 
-      if (profile) {
-        const cartItem = {
-          product,
-          unit: qty,
+    async FindCustomerById({ id }){
+
+        const existingCustomer = await CustomerModel.findById(id).populate('address');
+        // existingCustomer.cart = [];
+        // existingCustomer.orders = [];
+        // existingCustomer.wishlist = [];
+
+        // await existingCustomer.save();
+        return existingCustomer;
+    }
+
+    async Wishlist(customerId){
+
+        const profile = await CustomerModel.findById(customerId).populate('wishlist');
+       
+        return profile.wishlist;
+    }
+
+    async AddWishlistItem(customerId, { _id, name, desc, price, available, banner}){
+        
+        const product = {
+            _id, name, desc, price, available, banner
         };
 
-        let cartItems = profile.cart;
+        const profile = await CustomerModel.findById(customerId).populate('wishlist');
+       
+        if(profile){
 
-        if (cartItems.length > 0) {
-          let isExist = false;
-          cartItems.map((item) => {
-            if (item.product._id.toString() === product._id.toString()) {
-              if (isRemove) {
-                cartItems.splice(cartItems.indexOf(item), 1);
-              } else {
-                item.unit = qty;
-              }
-              isExist = true;
+             let wishlist = profile.wishlist;
+  
+            if(wishlist.length > 0){
+                let isExist = false;
+                wishlist.map(item => {
+                    if(item._id.toString() === product._id.toString()){
+                       const index = wishlist.indexOf(item);
+                       wishlist.splice(index,1);
+                       isExist = true;
+                    }
+                });
+
+                if(!isExist){
+                    wishlist.push(product);
+                }
+
+            }else{
+                wishlist.push(product);
             }
-          });
 
-          if (!isExist) {
-            cartItems.push(cartItem);
-          }
-        } else {
-          cartItems.push(cartItem);
+            profile.wishlist = wishlist;
         }
 
-        profile.cart = cartItems;
+        const profileResult = await profile.save();      
 
-        const cartSaveResult = await profile.save();
+        return profileResult.wishlist;
 
-        return cartSaveResult.cart;
-      }
-
-      throw new Error("Unable to add to cart!");
-    } catch (err) {
-      throw new APIError(
-        "API Error",
-        STATUS_CODES.INTERNAL_ERROR,
-        "Unable to Create Customer"
-      );
     }
-  }
 
-  async AddOrderToProfile(customerId, order) {
-    try {
-      const profile = await CustomerModel.findById(customerId);
 
-      if (profile) {
-        if (profile.orders == undefined) {
-          profile.orders = [];
+    async AddCartItem(customerId, { _id, name, price, banner},qty, isRemove){
+
+ 
+        const profile = await CustomerModel.findById(customerId).populate('cart');
+
+
+        if(profile){ 
+ 
+            const cartItem = {
+                product: { _id, name, price, banner },
+                unit: qty,
+            };
+          
+            let cartItems = profile.cart;
+            
+            if(cartItems.length > 0){
+                let isExist = false;
+                 cartItems.map(item => {
+                    if(item.product._id.toString() === _id.toString()){
+
+                        if(isRemove){
+                            cartItems.splice(cartItems.indexOf(item), 1);
+                        }else{
+                            item.unit = qty;
+                        }
+                        isExist = true;
+                    }
+                });
+
+                if(!isExist){
+                    cartItems.push(cartItem);
+                } 
+            }else{
+                cartItems.push(cartItem);
+            }
+
+            profile.cart = cartItems;
+
+            return await profile.save();
         }
-        profile.orders.push(order);
-
-        profile.cart = [];
-
-        const profileResult = await profile.save();
-
-        return profileResult;
-      }
-
-      throw new Error("Unable to add to order!");
-    } catch (err) {
-      throw new APIError(
-        "API Error",
-        STATUS_CODES.INTERNAL_ERROR,
-        "Unable to Create Customer"
-      );
+        
+        throw new Error('Unable to add to cart!');
     }
-  }
+
+
+
+    async AddOrderToProfile(customerId, order){
+ 
+        const profile = await CustomerModel.findById(customerId);
+
+        if(profile){ 
+            
+            if(profile.orders == undefined){
+                profile.orders = []
+            }
+            profile.orders.push(order);
+
+            profile.cart = [];
+
+            const profileResult = await profile.save();
+
+            return profileResult;
+        }
+        
+        throw new Error('Unable to add to order!');
+    }
+
+ 
+
+
 }
 
 module.exports = CustomerRepository;
